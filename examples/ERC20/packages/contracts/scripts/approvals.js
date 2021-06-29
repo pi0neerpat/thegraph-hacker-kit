070e0e2bc9fe6240f58ab49e2008d91143d84f89
dan13ram <dan13ram@users.noreply.github.com>
2021-06-29 22:50:17 +0530

    Update approvals.js

--- examples/ERC20/packages/contracts/scripts/approvals.js
+++ examples/ERC20/packages/contracts/scripts/approvals.js
@@ -29,19 +29,18 @@ async function main() {
     await token.mint(bob.address, utils.parseEther('2000'));
     console.log('minting 3000 to carol');
     await token.mint(carol.address, utils.parseEther('3000'));
+    // balances:
+    // alice: 1000 * 10^18 = 1000000000000000000000
+    // bob:   2000 * 10^18 = 2000000000000000000000
+    // carol: 3000 * 10^18 = 3000000000000000000000
+
+    console.log('alice transferring 10 to bob');
+    await token.connect(alice).transfer(bob.address, utils.parseEther('10'));
+    console.log('bob transferring 20 to carol');
+    await token.connect(bob).transfer(carol.address, utils.parseEther('20'));
+    console.log('carol transferring 30 to alice');
+    await token.connect(carol).transfer(alice.address, utils.parseEther('30'));
   }
-
-  // balances:
-  // alice: 1000 * 10^18 = 1000000000000000000000
-  // bob:   2000 * 10^18 = 2000000000000000000000
-  // carol: 3000 * 10^18 = 3000000000000000000000
-
-  console.log('alice transferring 10 to bob');
-  await token.connect(alice).transfer(bob.address, utils.parseEther('10'));
-  console.log('bob transferring 20 to carol');
-  await token.connect(bob).transfer(carol.address, utils.parseEther('20'));
-  console.log('carol transferring 30 to alice');
-  await token.connect(carol).transfer(alice.address, utils.parseEther('30'));
 
   // balances:
   // alice: 1000 - 10 + 30 = 1020 * 10^18 = 1020000000000000000000
